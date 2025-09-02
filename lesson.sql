2a7efd0fa1987bdf0aeea4759188ccdf0a83c8e1
BWuchiyama <tep317pei@gmail.com>
2025-09-02 19:27:15 +0900

    Update lesson.sql

--- lesson.sql
+++ lesson.sql
@@ -1,4 +1,6 @@
- insert into reports (person_id, content)
+
+Q1
+insert into reports (person_id, content)
     -> values(1, '鈴木たかしの日報'),
     -> (2, ' 田中ゆうこの日報'),
     -> (3, '福田だいすけの日報'),
@@ -8,3 +10,6 @@
     -> (8, '金城きよたかの日報'),
     -> (9, '佐藤たかとの日報'),
     -> (999, '加藤げんじの日報');
+
+Q2
+ ALTER TABLE people　ADD COLUMN department_id INT UNSIGNED NULL COMMENT '部署ID' AFTER email;
